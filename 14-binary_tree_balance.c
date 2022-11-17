b817072561ef5e558d9568accf78cd2078ed84a3
MercyKorir <mcthegreat78@gmail.com>
2022-11-18 00:14:16 +0300

    I have added the file 14*

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,12 +24,11 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height = 0, right_height = 0;
-
-        if (tree == NULL)
-                return (0);
-        left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        return ((left_height > right_height) ? 1 : right_height);
+	size_t left_height = 0, right_height = 0;
+	if (tree == NULL)
+		return (0);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((left_height > right_height) ? 1 : right_height);
 }
 
