90aee2af0776a204ab5d0448a7f088cb36980307
MercyKorir <mcthegreat78@gmail.com>
2022-11-18 14:20:19 +0300

    I have added file for 22*

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 	curr = tree->right;
-	temp = pivot->left;
+	temp = curr->left;
 	curr->left = tree;
 	tree->right = temp;
 	if (temp != NULL)
